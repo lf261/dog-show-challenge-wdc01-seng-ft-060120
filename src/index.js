2d9b2de15fc9aa2a3a3aeb22478881e2983a9b0d
lf261 <luis.fernandez2093@gmail.com>
2020-07-22 11:24:01 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
+//creating base urls below to refer to later
 
-})
+const BASE_URL = "http://localhost:3000/"
+const DOG_URL = `${BASE_URL}/dogs`
+
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+ //FOLLOW READ ME!!
+
+//first fetch 
+ 
+function getDogs() {
+ fetch("http://localhost:3000/dogs")
+    .then(res => res.json())
+    .then(dogs => {
+        console.log(dogs) //20 dogs populated in console
+    })
+}
+
+//document.addEventListener('click', submitButtonClicked)
+
+getDogs()
+
+});
+
+const renderDogs = (dog) =>
+{
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('td');
+
+    td1.textContent = `${dog.name}`
+    td2.textContent = `${dog.breed}`
+    td2.textContent = `${dog.sex}`
+
+}
+
+const submitButtonClicked = (e) => { 
+    const target = e.target
+
+}
